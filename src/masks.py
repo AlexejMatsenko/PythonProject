f3307847804672f05e34a8a880e65ad59fb9ea6f
Matsenko Alexej <matsenko.alexej@yandex.ru>
2026-01-29 17:55:23 +0300

    Создание функции mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,6 @@
-from typing import Union
+# from widget import *
 
-
-def get_mask_card_number(card_number: Union[int | str]) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты в виде числа и возвращает маску номера."""
     list_card_number = []
     for i in str(card_number):
@@ -16,9 +15,8 @@ def get_mask_card_number(card_number: Union[int | str]) -> str:
 
     return "".join(list_card_number)
 
-
-card_numbers = 7000792289606361
-print(get_mask_card_number(card_numbers))
+# print(get_mask_card_number(numbers))
+# print(mask_account_card(number))
 
 
 def get_mask_account(account_number: int | str) -> str:
@@ -30,5 +28,9 @@ def get_mask_account(account_number: int | str) -> str:
     return "".join(list_account_number[-6:])
 
 
-account_numbers = 73654108430135874305
-print(get_mask_account(account_numbers))
+# print(get_mask_account(""))
+
+# if __name__ == "__main__":
+#     print(get_mask_card_number(numbers))
+#     print(get_mask_account(numbers))
+
--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,26 @@
-def mask_account_card():
+from masks import get_mask_account, get_mask_card_number
 
-    return
+
+def mask_account_card(num_card_account: str):
+    """Функция принимает название карты,номер или счёт,"""
+    num_card_account_split = num_card_account.split()
+    number = num_card_account_split[-1]
+    card = []
+    for i in num_card_account_split:
+        if i.isalpha():
+            card.append(i)
+            if len(number) == 16:
+                numbers = get_mask_card_number(number)
+            elif len(number) == 20:
+                numbers = get_mask_account(number)
+    return f" {" ".join(card)} {numbers}"
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 7000792289606361"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(mask_account_card("Счет 64686473678894779589"))
