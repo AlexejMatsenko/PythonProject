86ec29e264b6af194e689710157aaa8a0f2dc329
Alexej Matsenko <matsenko.alexej@yandex.ru>
2026-03-14 15:31:53 +0300

    Корректировка функции и добавлен модуль test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,35 +1,47 @@
-from functools import wraps
+import functools
+from typing import Any, Callable, Optional
 
 
-def log(filename=None):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
+def log(filename: Optional[str] = None) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    """Декоратор логирует имя функции и результат выполнения операции,
+    так же при ошибке и входные параметры."""
+
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
-                log_information = f"{func.__name__} ok"
                 if filename:
                     with open(filename, "a", encoding="utf-8") as f:
-                        f.write(log_information + "\n")
+                        f.write(f"{func.__name__}: ok\n")
                 else:
-                    print(log_information)
+                    print(f"{func.__name__} ok")
                 return result
             except Exception as e:
-                log_information = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
                 if filename:
-                    with open(filename, "a") as f:
-                        f.write(log_information + "\n")
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n")
                 else:
-                    print(log_information)
-                raise
+                    print(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}")
 
         return wrapper
 
     return decorator
 
+
 @log(filename="mylog.txt")
-def my_function(x, y):
-    return x + y
+def my_function(x: int, y: int) -> Any:
+    """Функция вычесляет результат деления двух чисел"""
+    return x / y
 
 
+@log()
+def my_function1(x: int, y: int) -> Any:
+    return x / y
+
+
+my_function(4, 0)
 my_function(4, 2)
+my_function(
+    4,
+)
