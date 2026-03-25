89e4cebb23a5ca6de315dafd3a37de9a383dbbef
Alexej Matsenko <matsenko.alexej@yandex.ru>
2026-03-25 16:58:15 +0300

    Добавлен новый модуль с функцией логирования

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,9 @@
 from typing import Any
 
+from src.logging_config import setup_logging
+
+logger = setup_logging("masks")
+
 
 def get_mask_card_number(card_number: Any) -> Any:
     """Функция принимает на вход номер карты в виде числа и возвращает маску номера."""
@@ -9,9 +13,13 @@ def get_mask_card_number(card_number: Any) -> Any:
     for i in card_number:
 
         if i.isalpha():
+            logger.error("Номер состоит не только из цифр")
             raise ValueError("Номер должен состоять из цифр")
+
         if len(card_number) > 16 or len(card_number) < 16:
+            logger.error("Не соответствие количеству цифр номера карты")
             raise ValueError("Количество цифр карты должно быть 16")
+
         list_card_number.append(i)
         if len(list_card_number) == 4:
             list_card_number.append(" ")
@@ -20,7 +28,7 @@ def get_mask_card_number(card_number: Any) -> Any:
         elif len(list_card_number) == 14:
             list_card_number.append(" ")
             list_card_number[7:14] = ["*", "*", " ", "*", "*", "*", "*"]
-
+    logger.info("Успешное завершение маскировки")
     return "".join(list_card_number)
 
 
@@ -31,9 +39,18 @@ def get_mask_account(account_number: Any) -> Any:
         raise ValueError("Ничего не указано")
     for j in account_number:
         if j.isalpha():
+            logger.error("Счёт состоит не только из цифр")
             raise ValueError("Номер должен состоять из цифр")
+
         if len(account_number) > 20 or len(account_number) < 20:
+            logger.error("Не соответствие количеству цифр номера счёта")
             raise ValueError("Количество цифр карты должно быть 20")
         new_account_number.append(j)
     new_account_number[-6:-4] = ["*", "*"]
+    logger.info("Успешное завершение маскировки")
     return "".join(new_account_number[-6:])
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number("1234567891236540"))
+    print(get_mask_account("55565269854857231955"))
